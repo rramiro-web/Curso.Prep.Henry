561b3384b48db471534522048b6efea710bbe5d7
Ramiro Reche <reche.ramiro98@gmail.com>
2020-12-02 09:05:29 -0300

    finished the esPrimo exercise

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -89,14 +89,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  for(var i = 2; i < numero; i++) {
-    if(numero % i === 0) {
-      return false;
-    } else {
-      return true;
+  if(numero === 1 || numero === 0) return false;{
+    for(var i = 2; i < numero; i++) {
+        if(numero % i === 0) {
+          return false;
+        }
     }
-  }
+  } return true;
 }
+
 
 
 
